cfd659d0bb22136b62526d3200d2fd32a6a6e2c7
unknown <dlo21@pitt.edu>
2024-09-12 16:02:18 -0400

    yaml

--- server/server.js
+++ server/server.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const path = require("path");
 
+const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: ["http://localhost:5173"],
-};
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors(corsOptions));
-
-app.get("/api", (req, res) => {
-    res.json({ fruits: ["apple", "orange", "banana"] })
+// For any route not found in the API, send back the React index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// const cors = require("cors");
+
+
+// const corsOptions = {
+//     origin: ["http://localhost:5173"],
+// };
+
+// app.use(cors(corsOptions));
+
+// app.get("/api", (req, res) => {
+//     res.json({ fruits: ["apple", "orange", "banana"] })
+// });
+
+// app.listen(8080, () => {
+//     console.log("Server started on port 8080");
+// })
